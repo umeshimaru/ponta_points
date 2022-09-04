52de9d7f20ca25a7e897313931d61287c72a640f
Ubuntu <ubuntu@ip-172-31-44-116.ap-northeast-1.compute.internal>
2022-09-04 02:24:49 +0000

    points card

--- points.rb
+++ points.rb
@@ -1,15 +1,26 @@
+sum = 0 
+i = 0 
+
+while i < 1000000
 puts "金額を入力してください"
 money = gets.to_i 
+ponta_points = money / 100 
+sum += ponta_points
 
 if money >= 100
-  ponta_points = money / 100 
 puts "あなたの今回のﾎﾟｲﾝﾄは#{ponta_points}ﾎﾟｲﾝﾄです"
+puts "合計#{sum}ﾎﾟｲﾝﾄ保有"
 else money < 100 
-  puts "購入金額が１００円未満はポイントがつきません"
+  puts "購入金額が100円未満はﾎﾟｲﾝﾄがつきません"
+end 
+i +=1 
 end 
 
 
 
-#ポンタポイントの計算
-#金額を入力するとポイントを計算する
-# 100円で１ポイント
+
+#ﾎﾟﾝﾀﾎﾟｲﾝﾄの計算
+#金額を入力するとﾎﾟｲﾝﾄを計算する
+# 100円で1ﾎﾟｲﾝﾄ
+
+
